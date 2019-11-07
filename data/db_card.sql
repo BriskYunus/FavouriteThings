6ef84504491156a63ae74e478e845205b651fc09
Brisk Yunus <b_yunus@fanshaweonline.ca>
2019-11-07 12:25:00 -0500

    added database

--- data/db_card.sql
+++ data/db_card.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Nov 06, 2019 at 01:58 PM
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 07, 2019 at 05:22 PM
 -- Server version: 5.7.26
--- PHP Version: 7.3.8
+-- PHP Version: 7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -26,22 +28,24 @@ SET time_zone = "+00:00";
 -- Table structure for table `tbl_bio`
 --
 
-CREATE TABLE `tbl_bio` (
-  `ID` tinyint(4) NOT NULL,
+DROP TABLE IF EXISTS `tbl_bio`;
+CREATE TABLE IF NOT EXISTS `tbl_bio` (
+  `ID` tinyint(4) NOT NULL AUTO_INCREMENT,
   `profID` tinyint(4) DEFAULT NULL,
   `bio` varchar(200) DEFAULT NULL,
   `social` varchar(60) DEFAULT NULL,
-  `classes` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `classes` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_bio`
 --
 
 INSERT INTO `tbl_bio` (`ID`, `profID`, `bio`, `social`, `classes`) VALUES
-(1, 1, 'Pan is  a freakin code ninja. He knows WAAAAAAY to many things about code. It\'s a problem.', 'twitter, facebook, tik tok', 'MMED30sumthin Web Dev 3'),
-(2, 2, 'Trevor is a hard-core introvert who also loves teaching. He also really likes JavaScript, especially the shiny new full stack dev stuff.', 'twitter, facebook, snapchat, slack', 'MMED3012 Multimedia Authoring 3'),
-(3, 3, 'Justin loves fishing and outdoors stuff. He is also a fantastic teacher and works hard at constantly uprading the IDP program to be the best it can be.', 'facebook, snapchat', 'MMED1012 Web Design Fundamentals');
+(1, 1, 'Since I was very young I used to study music on the side cause it always stuck with me growing up. In high school I even started producing and mixing for local artist. View my work below!', 'Socials: WORKSOFBRISK', 'Fanshawe'),
+(2, 2, 'I always love playing soccer since Elementary School. I even still watch the world cup which always excites me each season.', 'ESPN', 'Fanshawe'),
+(3, 3, 'Tech was always a passion for me such as new phones, computers and devices. I also landed a job at Best Buy which I enjoy!', 'Best Buy', 'Fanshawe');
 
 -- --------------------------------------------------------
 
@@ -49,8 +53,9 @@ INSERT INTO `tbl_bio` (`ID`, `profID`, `bio`, `social`, `classes`) VALUES
 -- Table structure for table `tbl_card`
 --
 
-CREATE TABLE `tbl_card` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `tbl_card`;
+CREATE TABLE IF NOT EXISTS `tbl_card` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(60) COLLATE utf8_bin NOT NULL,
   `CompanyName` varchar(30) COLLATE utf8_bin NOT NULL,
   `Address` varchar(150) COLLATE utf8_bin NOT NULL,
@@ -61,49 +66,19 @@ CREATE TABLE `tbl_card` (
   `Phone` varchar(20) COLLATE utf8_bin NOT NULL,
   `Email` varchar(35) COLLATE utf8_bin NOT NULL,
   `Fax` varchar(20) COLLATE utf8_bin NOT NULL,
-  `avatar` varchar(30) COLLATE utf8_bin DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `avatar` varchar(30) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `tbl_card`
 --
 
 INSERT INTO `tbl_card` (`ID`, `Name`, `CompanyName`, `Address`, `Logo`, `URL`, `SocialMedia`, `JobTitle`, `Phone`, `Email`, `Fax`, `avatar`) VALUES
-(1, 'Pan', 'Fanshawe', '200 Dundas', 'https://www.fanshaweonline.ca/d2l/lp/navbars/29533/theme/viewimage/32508983/view?v=20.19.8.17536-327', 'https://www.fanshawec.ca', 'facebook', 'prof', '1112223333', 'b_pan2@fanshawec.ca', '111222333', 'pan.jpg'),
-(2, 'Trevor', 'Fanshawe', '200 Dundas', 'https://www.fanshaweonline.ca/d2l/lp/navbars/29533/theme/viewimage/32508983/view?v=20.19.8.17536-327', 'https://www.fanshawec.ca', 'facebook, twitter, snapchat', 'confuserator', '444555666', 'tvanrys@fanshaweonline.ca', '444555666', 'olaf.jpeg'),
-(3, 'Justin', 'Fanshawe', '200 Dundas', 'https://www.fanshaweonline.ca/d2l/lp/navbars/29533/theme/viewimage/32508983/view?v=20.19.8.17536-327', 'https://www.fanshawec.ca', 'kik, twitter, tik tok', 'coordinator', '7778889999', 'jbrunner@fanshaweonline.ca', '7778889999', 'justin.jpeg');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tbl_bio`
---
-ALTER TABLE `tbl_bio`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `tbl_card`
---
-ALTER TABLE `tbl_card`
-  ADD PRIMARY KEY (`ID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tbl_bio`
---
-ALTER TABLE `tbl_bio`
-  MODIFY `ID` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `tbl_card`
---
-ALTER TABLE `tbl_card`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+(1, 'Music', 'Fanshawe', '200 Dundas', 'https://www.fanshaweonline.ca/d2l/lp/navbars/29533/theme/viewimage/32508983/view?v=20.19.8.17536-327', 'https://www.apple.com/ca/music/', 'IG: worksofbrisk', 'Producer', '1112223333', 'b_yunus@fanshaweonline.ca', '111222333', 'music-icon.png'),
+(2, 'Soccer', 'Fanshawe', '200 Dundas', 'https://www.fanshaweonline.ca/d2l/lp/navbars/29533/theme/viewimage/32508983/view?v=20.19.8.17536-327', 'https://www.espn.com/soccer/scoreboard', 'ESPN or Facebook', 'Athlete', '444555666', 'b_yunus@fanshaweonline.ca', '444555666', 'soccer_icon.png'),
+(3, 'Tech', 'Fanshawe', '200 Dundas', 'https://www.fanshaweonline.ca/d2l/lp/navbars/29533/theme/viewimage/32508983/view?v=20.19.8.17536-327', 'https://www.bestbuy.ca/en-ca', 'BestBuy.ca', 'Advisor', '7778889999', 'b_yunus@fanshaweonline.ca', '7778889999', 'tech-icon.png');
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
